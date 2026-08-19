ac045f25caebb986043996de9f6b4720f8506d75
KSPOG <kratosmaximusosrs@gmail.com>
2026-08-19 19:09:24 +0200

    d
    d

--- f2pprocessingfactory/F2PProcessingFactoryPlugin.java
+++ f2pprocessingfactory/F2PProcessingFactoryPlugin.java
@@ -2,7 +2,6 @@ package net.runelite.client.plugins.microbot.f2pprocessingfactory;
 
 import com.google.gson.Gson;
 import com.google.inject.Provides;
-import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.plugins.Plugin;
@@ -25,12 +24,11 @@ import javax.inject.Inject;
 @Slf4j
 public class F2PProcessingFactoryPlugin extends Plugin
 {
-    public static final String VERSION = "1.0.38";
+    public static final String VERSION = "1.0.39";
 
     @Inject
     private F2PProcessingFactoryConfig config;
 
-    @Getter
     @Inject
     private F2PProcessingFactoryScript script;
 
@@ -45,6 +43,11 @@ public class F2PProcessingFactoryPlugin extends Plugin
 
     @Inject
     private Gson gson;
+
+    public F2PProcessingFactoryScript getScript()
+    {
+        return script;
+    }
 
     @Provides
     F2PProcessingFactoryConfig provideConfig(ConfigManager configManager)
