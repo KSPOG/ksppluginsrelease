5924e9293175e80ccb2f8d3f10e7b42caf02f4a6
KSPOG <kratosmaximusosrs@gmail.com>
2026-08-21 20:58:16 +0200

    Reduce AIO Fighter loot enum boilerplate

--- kspaiofighter/KspLootOwnership.java
+++ kspaiofighter/KspLootOwnership.java
@@ -1,16 +1,14 @@
 package net.runelite.client.plugins.microbot.kspaiofighter;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public enum KspLootOwnership
 {
 	LOOT_OWN("Loot Own"),
 	LOOT_ALL("Loot All");
 
 	private final String name;
-
-	KspLootOwnership(String name)
-	{
-		this.name = name;
-	}
 
 	@Override
 	public String toString()
