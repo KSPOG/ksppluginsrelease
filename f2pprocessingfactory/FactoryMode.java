e762f6c92b516b6eaa9fd456fafa94bdca96eaed
KSPOG <kratosmaximusosrs@gmail.com>
2026-08-21 20:54:08 +0200

    Reduce FactoryMode enum boilerplate

--- f2pprocessingfactory/FactoryMode.java
+++ f2pprocessingfactory/FactoryMode.java
@@ -1,16 +1,14 @@
 package net.runelite.client.plugins.microbot.f2pprocessingfactory;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public enum FactoryMode
 {
     AUTO_BEST_PROFIT("Automatic - best profitable recipe"),
     FIXED_RECIPE("Fixed recipe");
 
     private final String displayName;
-
-    FactoryMode(String displayName)
-    {
-        this.displayName = displayName;
-    }
 
     @Override
     public String toString()
