4083a7012f1ded3aaeef7af5370997282502ff13
KSPOG <kratosmaximusosrs@gmail.com>
2026-08-21 02:04:54 +0200

    Revert "agent: repair KspAutoRunPlugin [20260821-000208-234-KspAutoRunPlugin]"
    This reverts commit 0f2e32e9e7a161c3184d53e6bbd5830fb4f50410.

--- kspautorun/KspAutoRunScript.java
+++ kspautorun/KspAutoRunScript.java
@@ -300,8 +300,8 @@ public class KspAutoRunScript extends Script
 
     private String enabledState(int runEnergy, int threshold)
     {
-        return "run enabled for energy cycle " + confirmedRunEnableCycles
-                + " at " + runEnergy + "/" + threshold;
+        return "run enabled (cycle " + confirmedRunEnableCycles
+                + ", energy " + runEnergy + "/" + threshold + ")";
     }
 
     private void setMicrobotAutoRun(boolean enabled)
