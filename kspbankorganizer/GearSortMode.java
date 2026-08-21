f413f83414025c4417ae78c6628b37725b13e12c
KSPOG <kratosmaximusosrs@gmail.com>
2026-08-21 20:52:52 +0200

    Reduce GearSortMode enum boilerplate

--- kspbankorganizer/GearSortMode.java
+++ kspbankorganizer/GearSortMode.java
@@ -1,16 +1,14 @@
 package net.runelite.client.plugins.microbot.kspbankorganizer;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public enum GearSortMode
 {
     COMBAT_STYLE("Combat style"),
     EQUIPMENT_TYPE("Equipment type");
 
     private final String displayName;
-
-    GearSortMode(String displayName)
-    {
-        this.displayName = displayName;
-    }
 
     @Override
     public String toString()
