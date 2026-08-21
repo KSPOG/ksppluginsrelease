62b12fcf44066dbeb99b386375574f11378978e4
KSPOG <kratosmaximusosrs@gmail.com>
2026-08-21 19:41:54 +0200

    Generate render mode constructor

--- ksprenderdisable/RenderMode.java
+++ ksprenderdisable/RenderMode.java
@@ -1,16 +1,14 @@
 package net.runelite.client.plugins.microbot.ksprenderdisable;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public enum RenderMode
 {
     FREEZE_OUTPUT("Freeze renderer output"),
     REDUCE_RENDERING("Reduce rendering only");
 
     private final String displayName;
-
-    RenderMode(String displayName)
-    {
-        this.displayName = displayName;
-    }
 
     @Override
     public String toString()
