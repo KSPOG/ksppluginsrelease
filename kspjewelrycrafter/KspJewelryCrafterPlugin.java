dcc78e5d1ae780740dd10501d7ec8f62b4c2b3d5
KSPOG <kratosmaximusosrs@gmail.com>
2026-08-25 19:51:09 +0200

    Bump Jewelry Crafter to 0.1.8

--- kspjewelrycrafter/KspJewelryCrafterPlugin.java
+++ kspjewelrycrafter/KspJewelryCrafterPlugin.java
@@ -21,7 +21,7 @@ import javax.inject.Inject;
 )
 public class KspJewelryCrafterPlugin extends Plugin
 {
-    public static final String VERSION = "0.1.7";
+    public static final String VERSION = "0.1.8";
 
     @Inject private KspJewelryCrafterConfig config;
     @Inject private KspJewelryCrafterScript script;
