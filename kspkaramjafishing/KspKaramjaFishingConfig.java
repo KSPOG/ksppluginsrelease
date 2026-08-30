86e0d17e1bd5f51b2ce2b4171e54dff51e7cd277
KSPOG <kratosmaximusosrs@gmail.com>
2026-08-31 01:38:07 +0200

    Add shared mule controls to Karamja Fishing config

--- kspkaramjafishing/KspKaramjaFishingConfig.java
+++ kspkaramjafishing/KspKaramjaFishingConfig.java
@@ -3,49 +3,27 @@ package net.runelite.client.plugins.microbot.kspkaramjafishing;
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
+import net.runelite.client.config.ConfigSection;
+import net.runelite.client.plugins.microbot.kspmule.KspMuleConfig;
 
 @ConfigGroup("kspKaramjaFishing")
-public interface KspKaramjaFishingConfig extends Config
+public interface KspKaramjaFishingConfig extends Config, KspMuleConfig
 {
+    @ConfigSection(name = "Local Mule", description = "Automatic excess-GP transfer to KSP Trade Receiver", position = 90)
+    String muleSection = KspMuleConfig.SECTION;
+
     enum Mode
     {
         TUNA_SWORDFISH("Tuna & Swordfish", "Harpoon"),
         LOBSTER("Lobster", "Cage");
-
         private final String name;
         private final String action;
-
-        Mode(String name, String action)
-        {
-            this.name = name;
-            this.action = action;
-        }
-
-        public String getName()
-        {
-            return name;
-        }
-
-        public String getAction()
-        {
-            return action;
-        }
-
-        @Override
-        public String toString()
-        {
-            return name;
-        }
+        Mode(String name, String action) { this.name = name; this.action = action; }
+        public String getName() { return name; }
+        public String getAction() { return action; }
+        @Override public String toString() { return name; }
     }
 
-    @ConfigItem(
-            keyName = "mode",
-            name = "Fish",
-            description = "Tuna & Swordfish uses a harpoon. Lobster uses a lobster pot.",
-            position = 0
-    )
-    default Mode mode()
-    {
-        return Mode.TUNA_SWORDFISH;
-    }
+    @ConfigItem(keyName = "mode", name = "Fish", description = "Tuna & Swordfish uses a harpoon. Lobster uses a lobster pot.", position = 0)
+    default Mode mode() { return Mode.TUNA_SWORDFISH; }
 }
