1a19b6cd3d57155c2f7a785e41ba13042a85da5a
KSPOG <kratosmaximusosrs@gmail.com>
2026-09-17 22:50:46 +0200

    Bump F2P Gathering Profit for Source Loader compatibility fix

--- kspf2pgatheringprofit/KspF2pGatheringProfitPlugin.java
+++ kspf2pgatheringprofit/KspF2pGatheringProfitPlugin.java
@@ -22,7 +22,7 @@ import java.time.Instant;
 )
 public class KspF2pGatheringProfitPlugin extends Plugin
 {
-    public static final String VERSION = "0.0.3";
+    public static final String VERSION = "0.0.4";
 
     @Inject private KspF2pGatheringProfitConfig config;
     @Inject private KspF2pGatheringProfitScript script;
