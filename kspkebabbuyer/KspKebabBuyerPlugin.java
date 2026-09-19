6a57a3a6d1122556020c22b3f53cfbc50bfa28aa
KSPOG <kratosmaximusosrs@gmail.com>
2026-09-20 00:45:07 +0200

    Add KSP Kebab Buyer plugin

---
+++ kspkebabbuyer/KspKebabBuyerPlugin.java
@@ -0,0 +1,51 @@
+package net.runelite.client.plugins.microbot.kspkebabbuyer;
+
+import net.runelite.client.plugins.Plugin;
+import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.plugins.microbot.PluginConstants;
+import net.runelite.client.ui.overlay.OverlayManager;
+
+import javax.inject.Inject;
+
+@PluginDescriptor(
+        name = PluginConstants.KSP + "Kebab Buyer",
+        description = "Buys kebabs from Karim in Al Kharid and banks them automatically.",
+        tags = {"ksp", "kebab", "karim", "al kharid", "money making", "f2p"},
+        authors = {"KSP"},
+        version = KspKebabBuyerPlugin.VERSION,
+        minClientVersion = "2.6.19",
+        enabledByDefault = PluginConstants.DEFAULT_ENABLED,
+        isExternal = PluginConstants.IS_EXTERNAL
+)
+public class KspKebabBuyerPlugin extends Plugin
+{
+    public static final String VERSION = "0.0.1";
+
+    @Inject
+    private KspKebabBuyerScript script;
+
+    @Inject
+    private KspKebabBuyerOverlay overlay;
+
+    @Inject
+    private OverlayManager overlayManager;
+
+    @Override
+    protected void startUp()
+    {
+        overlayManager.add(overlay);
+        script.run();
+    }
+
+    @Override
+    protected void shutDown()
+    {
+        script.shutdown();
+        overlayManager.remove(overlay);
+    }
+
+    KspKebabBuyerScript getScript()
+    {
+        return script;
+    }
+}
