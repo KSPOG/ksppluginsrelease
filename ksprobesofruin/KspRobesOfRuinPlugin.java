33bc0b1ac6b7996b790a43c08edd95394c9bf460
KSPOG <kratosmaximusosrs@gmail.com>
2026-09-19 05:48:12 +0200

    Fix Blow Kiss player animation tracking

--- ksprobesofruin/KspRobesOfRuinPlugin.java
+++ ksprobesofruin/KspRobesOfRuinPlugin.java
@@ -43,7 +43,7 @@ import net.runelite.client.ui.overlay.OverlayManager;
 )
 public class KspRobesOfRuinPlugin extends Plugin
 {
-    public static final String VERSION = "0.0.7";
+    public static final String VERSION = "0.0.8";
 
     // Exact first Robes of Ruin dig tile confirmed in-game.
     static final WorldPoint LUMBRIDGE_DIG_TILE = new WorldPoint(3190, 3165, 0);
@@ -152,7 +152,7 @@ public class KspRobesOfRuinPlugin extends Plugin
             Map.entry(857, Emote.THINK),
             Map.entry(866, Emote.DANCE),
             Map.entry(5316, Emote.DANCE),
-            Map.entry(1368, Emote.BLOW_KISS),
+            Map.entry(1374, Emote.BLOW_KISS),
             Map.entry(863, Emote.WAVE),
             Map.entry(858, Emote.BOW),
             Map.entry(2108, Emote.HEADBANG),
