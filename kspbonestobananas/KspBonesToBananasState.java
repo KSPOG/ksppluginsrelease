8494740309099be469588278bbc45ea09c96415e
KSPOG <kratosmaximusosrs@gmail.com>
2026-09-21 12:35:12 +0200

    Add KSP Bones to Bananas core

---
+++ kspbonestobananas/KspBonesToBananasState.java
@@ -0,0 +1,14 @@
+package net.runelite.client.plugins.microbot.kspbonestobananas;
+
+public enum KspBonesToBananasState
+{
+    STARTING,
+    SCANNING_MARKET,
+    PREPARING_BATCH,
+    CASTING,
+    RESTOCKING,
+    SELLING_OUTPUT,
+    WAITING_FOR_PROFIT,
+    ERROR,
+    STOPPED
+}
